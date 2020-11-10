72075cb90a4031485fbf16ff978b464196284c75
YoSindiOrtega30 <71533300+YoSindiOrtega30@users.noreply.github.com>
2020-11-10 02:01:01 -0500

    Add files via upload
    Enel commit anterior se agregan las dos vistras principales 1 "index" y 2 "recibe" . acompañadas con archivos segmetos de código. 
    En este se agrega archivos css y js.

---
+++ js.js
@@ -0,0 +1,13 @@
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+});
